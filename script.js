94351c66f721a440650a7c106c79cd04557c7e9d
vikapitoshka <kapitonenko.vick@ya.ru>
2018-02-07 14:06:22 +0300

    output 18 cards with pair

--- script.js
+++ script.js
@@ -6,32 +6,68 @@ var app = new Vue({
   },
   methods: {
   	check: function (num) {
-  	  //alert(this.cards[num].image);
-  	  
+      //alert( typeof(num) );
+      //alert(num);
+  	  alert(this.cards[num]);// число берёт из массива объектор cards, больше 18 не примет, вернёт undefined
+  	  //alert(this.cards[num].suit);
   	}
   }
 });
-var app2 = new Vue({
+/*var app2 = new Vue({
   el: '#app2',
   data: {
     allCards: [
     ]
   }
-});
+});*/
 
-let suits = ['H', 'D', 'S', 'C']; // 4
-let names = ['2', '3', '4', '5', '6', '7', '8', '9', '0', 'J', 'Q', 'K', 'A']; // 13
-numbers = -1;
+// all data
+let allSuits = ['H', 'D', 'S', 'C']; // 4
+let allNames = ['2', '3', '4', '5', '6', '7', '8', '9', '0', 'J', 'Q', 'K', 'A']; // 13
+//let numb = 0;
+
+
+// array for all cards
+const suits = [];
+const names = [];
+const images = [];
+//const numbers = [];
+
 for (let i = 0; i < 13; i++) {
 	for (let k = 0; k < 4; k++) {
-		numbers = numbers + 1;
-		app2.allCards.push({ suit: suits[k], number: numbers, name: names[i], image: 'Cards/' + names[i] + suits[k] + '.png' })
+    suits.push(allSuits[k]);
+    names.push(allNames[i]);
+    images.push('Cards/' + allNames[i] + allSuits[k] + '.png');
+		//app2.allCards.push({ suit: suits[k], number: numbers, name: names[i], image: 'Cards/' + names[i] + suits[k] + '.png' });
 	}	
 }
 
-// нужно случайные 9 карт
+/*for (let j = 0; j < 52; j++) {
+  numbers.push(j);
+}
+*/
+// вывод всех карт
+/*for (let x = 0; x < 52; x++) {
+  app.cards.push({ suit: suits[x], name: names[x], image: images[x], });
+}*/
+
+
+// случайные 9 карт по паре в перемешку
+let rands = [];
 for (let l = 0; l < 9; l++) {
 	let rand = Math.floor(Math.random() * (52 - 1 + 1)) + 1;
-	app.cards.push(app2.allCards[rand]);
-	app.cards.push(app2.allCards[rand]);
+  rands.push(rand);
+  rands.push(rand);	
 }
+
+function compareRandom() {
+  return Math.random() - 0.5;
+}
+
+rands.sort(compareRandom); // перемешиваем индексы карт
+for (z = 0; z < rands.length; z++) {
+  app.cards.push({ suit: suits[rands[z]], name: names[rands[z]], image: images[rands[z]], number: rands[z] });
+}
+
+//app.cards.push({ suit: app2.allCards[rand].suit, number: app2.allCards[rand].number, name: app2.allCards[rand].name, image: app2.allCards[rand].image });
+//app.cards.push({ suit: app2.allCards[rand].suit, number: app2.allCards[rand].number, name: app2.allCards[rand].name, image: app2.allCards[rand].image });
